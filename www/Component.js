1aed1d97be4f83e4908deb0bd1ba37a77229c564
Ivan Ignacio Vera Lynch <ivan.vera@neoris.cxnetworks.net>
2018-08-06 19:48:40 -0300

    Primer commit

---
+++ www/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+	"sap/ui/core/UIComponent",
+	"sap/ui/model/json/JSONModel"
+
+], function (UIComponent, JSONModel) {
+	"use strict";
+	return UIComponent.extend("pae.logistica.Component", {
+		metadata : {
+			manifest : "json"
+		}
+	});
+});
