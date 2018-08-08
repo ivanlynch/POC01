bbd35121a2ae23716071f25be5ae1608253004a8
Ivan Ignacio Vera Lynch <ivan.vera@neoris.cxnetworks.net>
2018-08-08 19:07:21 -0300

    Se agrego el controlador para la camara

--- www/controller/Home.controller.js
+++ www/controller/Home.controller.js
@@ -1,7 +1,50 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-], function (Controller) {
+	"sap/ui/model/json/JSONModel"
+], function (Controller, JSONModel) {
 	"use strict";
+	var imageRoute;
+
 	return Controller.extend("pae.logistica.controller.Home", {
+		onInit: function(){
+			var data = {
+				fechaActual: new Date()
+			}
+			var oModel = new sap.ui.model.json.JSONModel();
+			oModel.setData(data);
+			sap.ui.getCore().setModel(oModel);
+		},
+		onCamera: function(){
+
+			document.addEventListener("deviceready", onDeviceReady, false);
+			
+			var pictureSource, destinationType;
+
+			function onDeviceReady() {
+				pictureSource = navigator.camera.PictureSourceType;
+				destinationType = navigator.camera.DestinationType;
+			}
+
+			function TomarFoto() {
+				navigator.camera.getPicture(onSuccess, onFail, {
+					quality: 50,
+					destinationType: destinationType.DATA_URL,
+					saveToPhotoAlbum: true
+				});
+			}
+
+			function onSuccess(imageData) {
+				var smallImage = document.getElementById('smallImage');
+				smallImage.style.display = 'block';
+				smallImage.src = "data:image/jpeg;base64," + imageData;
+			}
+
+
+			function onFail(){
+				alert('Failed because: ' + message);
+			}
+
+			return TomarFoto();
+		}
 	});
 });
