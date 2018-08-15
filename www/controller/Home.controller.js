1ec48f98daab082501141753ce81934172648971
Ivan Ignacio Vera Lynch <ivan.vera@neoris.cxnetworks.net>
2018-08-15 18:27:57 -0300

    Se creo una rama nueva donde se va a desarrollar la parte de persistencia y consumicion de servicios

--- www/controller/Home.controller.js
+++ www/controller/Home.controller.js
@@ -14,52 +14,6 @@ sap.ui.define([
 			var oModel = new sap.ui.model.json.JSONModel();
 			oModel.setData(data);
 			sap.ui.getCore().setModel(oModel);
-		},
-		onCamera: function(){
-
-			document.addEventListener("deviceready", onDeviceReady, false);
-			
-			var pictureSource, destinationType;
-
-			function onDeviceReady() {
-				pictureSource = navigator.camera.PictureSourceType;
-				destinationType = navigator.camera.DestinationType;
-			}
-
-			function TomarFoto() {
-				navigator.camera.getPicture(onSuccess, onFail, {
-					quality: 50,
-					destinationType: destinationType.FILE_URI,
-					saveToPhotoAlbum: true
-				});
-			}
-
-			function onSuccess(imageData) {
-				var smallImage = document.getElementById('smallImage');
-				smallImage.src = "data:image/jpeg;base64," + imageData;
-			}
-
-
-			function onFail(){
-				alert('Failed because: ' + message);
-			}
-
-			return TomarFoto();
-		},
-		onSelectRadioButtonGroup: function(oEvent){
-			if(oEvent.getSource().getSelectedButton().getText() != "Confidencial"){
-				sap.ui.getCore().byId("__component0---Home--formConfidencial").setVisible(false);
-			}else{
-				sap.ui.getCore().byId("__component0---Home--formConfidencial").setVisible(true);
-			}
-		},
-		onGuardar: function(){
-			MessageToast.show("Su imágen ha sido almacenada en la memoria del teléfono", {
-				duration: 1500,
-				autoClose: false
-			});
-			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-				oRouter.navTo("App", true);
 		}
-	});
+	})
 });
